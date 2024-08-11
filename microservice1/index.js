40e6b3ad8edd4a9d1f0e0f44af509dc1de0e756f
Prashant Kumar Dey <prashantsavior@gmail.com>
2024-08-11 15:23:16 +0530

    Update index.js

--- microservice1/index.js
+++ microservice1/index.js
@@ -9,25 +9,21 @@ app.get('/', (req,res)=>{
     res.send('Hello World!')
 })
 
-app.get('/ms1/user', (req,res)=>{
-    res.send('Hello World from MS1 User!')
-})
 
 
+// app.get('/ms2', async (req,res)=>{
+//     const apiRes = await fetch(`${process.env.URL}:3001/user`)
+//     const apiResJson = await apiRes.json()
+//     console.log(apiResJson)    
+//     res.send(apiResJson)
+// })
 
-app.get('/ms2', async (req,res)=>{
-    const apiRes = await fetch(`${process.env.URL}:3001/user`)
-    const apiResJson = await apiRes.json()
-    console.log(apiResJson)    
-    res.send(apiResJson)
-})
-
-app.get('/ms3', async (req,res)=>{
-    const apiRes = await fetch(`${process.env.URL}:3002/user`)
-    const apiResJson = await apiRes.json()
-    console.log(apiResJson)
-    res.send(apiResJson)
-})
+// app.get('/ms3', async (req,res)=>{
+//     const apiRes = await fetch(`${process.env.URL}:3002/user`)
+//     const apiResJson = await apiRes.json()
+//     console.log(apiResJson)
+//     res.send(apiResJson)
+// })
 
 app.listen(3000, ()=>{
     console.log('Microservice 1 is listening at port 3000')
